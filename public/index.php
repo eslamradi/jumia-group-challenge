aa5ab190d20265d5dfabf8e6b7e77308ca725114
radi <eslamradii@gmail.com>
2022-04-09 13:11:32 +0200

    set up application starting script

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ .'/../vendor/autoload.php';
+
+use Core\App;
+use Core\Container;
+use Core\Lib\Request;
+
+// create new service container
+$container = new Container();
+
+
+// attach app config instance to the container
+$config = require_once __DIR__ .'/../app/config.php';
+$config($container);
+
+// attach other app dependencies to the container
+$dependencies = require_once __DIR__ .'/../app/dependencies.php';
+$dependencies($container);
+
+// build the app
+$app = new App($container);
+
+// attach application routes
+$routes = require_once __DIR__ .'/../app/routes.php';
+$routes($app);
+
+
+// handle an incoming Http Rquest 
+$response = $app->handle(new Request);
+
+return $response;
